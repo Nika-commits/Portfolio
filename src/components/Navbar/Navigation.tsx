c24e3f3595cff0ea99a2d81d046a557ed75d85b9
Your Name <itismepranish@gmail.com>
2026-06-10 21:28:55 +0545

    made nav sticky

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export default function Navigation() {
 	return (
-		<nav className="grid grid-cols-3 py-4 h-fit md:h-30 ">
+		<nav className="grid grid-cols-3 py-4 h-fit md:h-30 sticky top-0 z-50">
 			<div className="flex justfy-center items-center  ">{/* <Oggy /> */}</div>
 			<div className="flex justify-center items-center sticky top-0 z-50">
 				<NavigationTabs />
