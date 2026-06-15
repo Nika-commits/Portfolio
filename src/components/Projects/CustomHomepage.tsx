73016a52c885e621d4200c2ec68304271b4de652
Your Name <itismepranish@gmail.com>
2026-06-15 18:06:50 +0545

    chore: added 1 proje

--- src/components/Projects/CustomHomepage.tsx
+++ src/components/Projects/CustomHomepage.tsx
@@ -1,4 +1,5 @@
 import { Globe, Link } from "lucide-react";
+import { useState } from "react";
 import { UnderlineLink } from "../Global/UnderlineLink";
 import { Button } from "../ui/button";
 import {
@@ -8,58 +9,64 @@ import {
 	CardHeader,
 	CardTitle,
 } from "../ui/card";
+import { Dialog, DialogClose, DialogContent, DialogFooter } from "../ui/dialog";
+
+const link =
+	"https://gqtuuqsgkyffgcpbfltk.supabase.co/storage/v1/object/public/Portfolio/custom-tab.webp";
 
 export default function CustomHomepage() {
+	const [open, setOpen] = useState(false);
 	return (
-		<Card className="project-card">
-			<CardHeader>
-				<CardTitle className="flex justify-between items-center">
-					<UnderlineLink to="/">
-						<span className="flex gap-4 items-center">
-							Quizex <Link size={14} />
-						</span>
-					</UnderlineLink>
-					<div>
-						<a
-							href="https://tapwholesalenepal.com"
-							target="_blank"
-							rel="noopener"
-						>
-							<Button>
-								<Globe />
-								Live
-							</Button>
-						</a>
-					</div>
-				</CardTitle>
-				<CardDescription>
-					Tap Whole Sale AI is an AI-powered platform that
-				</CardDescription>
-			</CardHeader>
-			<CardContent className="flex flex-col space-y-4">
-				<div className="flex justify-center gap-1">
-					<img
-						width={200}
-						height={200}
-						src="https://gqtuuqsgkyffgcpbfltk.supabase.co/storage/v1/object/public/Portfolio/tapwholesale.webp"
-						alt="Tap Whole Sale AI"
-					/>
+		<>
+			<Card className="project-card">
+				<CardHeader>
+					<CardTitle className="flex justify-between items-center">
+						<UnderlineLink to="/">
+							<span className="flex gap-4 items-center">
+								Quizex <Link size={14} />
+							</span>
+						</UnderlineLink>
+						<div>
+							<a
+								href="https://tapwholesalenepal.com"
+								target="_blank"
+								rel="noopener"
+							>
+								<Button>
+									<Globe />
+									Live
+								</Button>
+							</a>
+						</div>
+					</CardTitle>
+					<CardDescription>
+						Tap Whole Sale AI is an AI-powered platform that
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="flex flex-col space-y-4">
+					<button type="button" onClick={() => setOpen(true)}>
+						<div className="flex justify-center gap-1">
+							<img
+								width={600}
+								height={600}
+								src={link}
+								alt="Tap Whole Sale AI"
+							/>
+						</div>
+					</button>
+				</CardContent>
+			</Card>
 
-					<img
-						width={200}
-						height={200}
-						src="https://gqtuuqsgkyffgcpbfltk.supabase.co/storage/v1/object/public/Portfolio/tapwholesale-prod.webp"
-						alt="Tap Whole Sale AI"
-					/>
-				</div>
-
-				{/*<div className="flex justify-center">
-					<UnderlineWrapper>
-						<Link to="/">Learn More</Link>
-					</UnderlineWrapper>
-				</div>*/}
-			</CardContent>
-			{/*<CardFooter></CardFooter>*/}
-		</Card>
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogContent className="max-w-2xl! ">
+					<img src={link} alt="Tap Whole Sale AI" />
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="outline">Close</Button>
+						</DialogClose>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
 	);
 }
