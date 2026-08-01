69935466dbbf629768c55d07ba6a0c5e778db88a
Your Name <itismepranish@gmail.com>
2026-08-01 15:12:29 +0545

    minor

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -8,26 +8,39 @@ export default function Navigation() {
     return (
         <nav className="grid grid-cols-8 items-center py-4 sticky top-0 z-50 myNav">
             <div className="col-span-2 flex justify-start items-center">
-                <div className="social-links grid grid-cols-3 gap-4 place-items-center ">
+                <div className="social-links grid grid-cols-3 gap-5 items-end ">
                     <UnderlineWrapper
                         href="https://www.linkedin.com/in/pranish-chaulagain-0a4833300/"
-                        className="text-xl md:text-xl font-extrabold"
+                        className="flex flex-col items-center"
                     >
-                        in
+                        <span className="text-xl md:text-xl font-extrabold align-bottom">
+                            in
+                        </span>
+                        {/*<span>LinkedIn</span>*/}
                     </UnderlineWrapper>
 
-                    <UnderlineWrapper href="https://github.com/Nika-commits/">
+                    <UnderlineWrapper
+                        href="https://github.com/Nika-commits/"
+                        className="flex flex-col self-center items-center"
+                    >
                         <SimpleIcon
                             icon={siGithub}
+                            className="size-5"
                             overrideColor="gray"
                         />
+                        {/*<span>Github</span>*/}
                     </UnderlineWrapper>
 
-                    <UnderlineWrapper href="https://www.instagram.com/_pranisshh/">
+                    <UnderlineWrapper
+                        href="https://www.instagram.com/_pranisshh/"
+                        className="flex flex-col self-center items-center"
+                    >
                         <SimpleIcon
                             icon={siInstagram}
+                            className="size-5 "
                             overrideColor="gray"
                         />
+                        {/*<span>Instagram</span>*/}
                     </UnderlineWrapper>
                 </div>
             </div>
