8f11c854a8a39fda3dd925c9503a9a424e760d87
Your Name <itismepranish@gmail.com>
2026-08-01 15:05:10 +0545

    minor

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -5,42 +5,40 @@ import NavigationTabs from "./NavigationTabs";
 import { ThemeToggle } from "./ThemeToggle";
 
 export default function Navigation() {
-	return (
-		<nav className="grid grid-cols-8 items-center py-4 sticky top-0 z-50 myNav">
-			<div className="col-span-2 flex justify-start items-center">
-				<div className="social-links grid grid-cols-3 gap-4 place-items-center ">
-					<UnderlineWrapper
-						href="https://www.linkedin.com/in/pranish-chaulagain-0a4833300/"
-						className="text-xl md:text-xl font-extrabold"
-					>
-						in
-					</UnderlineWrapper>
+    return (
+        <nav className="grid grid-cols-8 items-center py-4 sticky top-0 z-50 myNav">
+            <div className="col-span-2 flex justify-start items-center">
+                <div className="social-links grid grid-cols-3 gap-4 place-items-center ">
+                    <UnderlineWrapper
+                        href="https://www.linkedin.com/in/pranish-chaulagain-0a4833300/"
+                        className="text-xl md:text-xl font-extrabold"
+                    >
+                        in
+                    </UnderlineWrapper>
 
-					<UnderlineWrapper href="https://github.com/Nika-commits/">
-						<SimpleIcon
-							icon={siGithub}
-							className="size-5"
-							overrideColor="gray"
-						/>
-					</UnderlineWrapper>
+                    <UnderlineWrapper href="https://github.com/Nika-commits/">
+                        <SimpleIcon
+                            icon={siGithub}
+                            overrideColor="gray"
+                        />
+                    </UnderlineWrapper>
 
-					<UnderlineWrapper href="https://www.instagram.com/_pranisshh/">
-						<SimpleIcon
-							icon={siInstagram}
-							className="size-4 "
-							overrideColor="gray"
-						/>
-					</UnderlineWrapper>
-				</div>
-			</div>
+                    <UnderlineWrapper href="https://www.instagram.com/_pranisshh/">
+                        <SimpleIcon
+                            icon={siInstagram}
+                            overrideColor="gray"
+                        />
+                    </UnderlineWrapper>
+                </div>
+            </div>
 
-			<div className="col-span-4 flex justify-center items-center">
-				<NavigationTabs />
-			</div>
+            <div className="col-span-4 flex justify-center items-center">
+                <NavigationTabs />
+            </div>
 
-			<div className="col-span-2 flex justify-end items-center social-links">
-				<ThemeToggle />
-			</div>
-		</nav>
-	);
+            <div className="col-span-2 flex justify-end items-center social-links">
+                <ThemeToggle />
+            </div>
+        </nav>
+    );
 }
