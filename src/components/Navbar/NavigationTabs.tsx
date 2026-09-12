58b0d4e2b51b89b0866ad328a9283ee38928dca2
Your Name <itismepranish@gmail.com>
2026-09-12 20:29:09 +0545

    style: Refactored Old Codes

--- src/components/Navbar/NavigationTabs.tsx
+++ src/components/Navbar/NavigationTabs.tsx
@@ -1,80 +1,80 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useLocation, useNavigate } from "@tanstack/react-router";
 import {
-	BriefcaseIcon,
-	FolderKanbanIcon,
-	HomeIcon,
-	NotebookPenIcon,
+    BriefcaseIcon,
+    FolderKanbanIcon,
+    HomeIcon,
+    NotebookPenIcon,
 } from "lucide-react";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { GradientText } from "../animate-ui/primitives/texts/gradient";
 
 const NAV_LINKS = [
-	{ label: "Home", path: "/", icon: <HomeIcon className="size-4 shrink-0" /> },
-	{
-		label: "Work",
-		path: "/work",
-		icon: <BriefcaseIcon className="size-4 shrink-0" />,
-	},
-	{
-		label: "Projects",
-		path: "/projects",
-		icon: <FolderKanbanIcon className="size-4 shrink-0" />,
-	},
-	{
-		label: "Blogs",
-		path: "/blogs",
-		icon: <NotebookPenIcon className="size-4 shrink-0" />,
-	},
+    { label: "Home", path: "/", icon: <HomeIcon className="size-4 shrink-0" /> },
+    {
+        label: "Work",
+        path: "/work",
+        icon: <BriefcaseIcon className="size-4 shrink-0" />,
+    },
+    {
+        label: "Projects",
+        path: "/projects",
+        icon: <FolderKanbanIcon className="size-4 shrink-0" />,
+    },
+    {
+        label: "Blogs",
+        path: "/blogs",
+        icon: <NotebookPenIcon className="size-4 shrink-0" />,
+    },
 ];
 
 const PATHS = NAV_LINKS.map((l) => l.path);
 
 export default function NavigationTabs() {
-	const navigate = useNavigate();
-	const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
 
-	return (
-		<div className="flex fixed items-center justify-center bottom-[2dvh] lg:static ">
-			<Tabs
-				value={pathname}
-				className="w-full bg-background border-2 border-primary/30 rounded-3xl"
-			>
-				<TabsList className="p-2 gap-6 rounded-3xl">
-					{NAV_LINKS.map(({ label, path, icon }) => {
-						const isActive = pathname === path;
+    return (
+        <div className="flex fixed left-1/2 z-50 -translate-x-1/2 lg:translate-x-0 items-center justify-center bottom-[2dvh] lg:static ">
+            <Tabs
+                value={pathname}
+                className="bg-background border-2 border-primary/30 rounded-3xl"
+            >
+                <TabsList className="p-2 gap-6 rounded-3xl">
+                    {NAV_LINKS.map(({ label, path, icon }) => {
+                        const isActive = pathname === path;
 
-						return (
-							<TabsTrigger
-								key={label}
-								value={path}
-								className="h-9 gap-1.5 rounded-3xl cursor-pointer border-2 border-secondary  "
-								onClick={() => {
-									const currentIndex = PATHS.indexOf(pathname);
-									const nextIndex = PATHS.indexOf(path);
-									navigate({
-										to: path,
-										viewTransition: {
-											types:
-												currentIndex > nextIndex
-													? ["slide-right"]
-													: ["slide-left"],
-										},
-									});
-								}}
-							>
-								{icon}
-								{isActive && (
-									<GradientText
-										// gradient="var(--gradient-primary)"
-										text={label}
-										className="text-sm font-medium whitespace-nowrap"
-									/>
-								)}
-							</TabsTrigger>
-						);
-					})}
-				</TabsList>
-			</Tabs>
-		</div>
-	);
+                        return (
+                            <TabsTrigger
+                                key={label}
+                                value={path}
+                                className="h-9 gap-1.5 rounded-3xl cursor-pointer border-2 border-secondary  "
+                                onClick={() => {
+                                    const currentIndex = PATHS.indexOf(pathname);
+                                    const nextIndex = PATHS.indexOf(path);
+                                    navigate({
+                                        to: path,
+                                        viewTransition: {
+                                            types:
+                                                currentIndex > nextIndex
+                                                    ? ["slide-right"]
+                                                    : ["slide-left"],
+                                        },
+                                    });
+                                }}
+                            >
+                                {icon}
+                                {isActive && (
+                                    <GradientText
+                                        // gradient="var(--gradient-primary)"
+                                        text={label}
+                                        className="text-sm font-medium whitespace-nowrap"
+                                    />
+                                )}
+                            </TabsTrigger>
+                        );
+                    })}
+                </TabsList>
+            </Tabs>
+        </div>
+    );
 }
